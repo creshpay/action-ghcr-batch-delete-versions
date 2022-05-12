6f56dd4307dc959a5d4a7ee0b94bd11fa4dba7d8
maniolias <cedric@cresh.eu>
2022-05-12 11:15:16 +0200

    feat: implement retryer if github returns an error

---
+++ src/helpers/retry.test.js
@@ -0,0 +1,63 @@
+const { retry } = require("./retry")
+
+describe('Retryer', () => {
+    describe('#retry', () => {
+        test('Should return the response', async () => {
+            // Arrange
+            const p = () => Promise.resolve('success')
+            // Act
+            const res = await retry(p)
+            // Assert
+            expect(res).toEqual('success')
+        })
+
+        test('Should retry until success (max=5)', async () => {
+            // Arrange
+            let counter = 0
+            const successWhen = 4
+            const p = () => new Promise((resolve, reject) => {
+                if (counter === successWhen) {
+                    resolve('success')
+                }
+                counter++
+                reject('error')
+            })
+            // Act
+            const res = await retry(p)
+            // Assert
+            expect(res).toEqual('success')
+        })
+
+        test('Should retry until success (max=10)', async () => {
+            // Arrange
+            let counter = 0
+            const successWhen = 6
+            const p = () => new Promise((resolve, reject) => {
+                if (counter === successWhen) {
+                    resolve('success')
+                }
+                counter++
+                reject('error')
+            })
+            // Act
+            const res = await retry(p, 10)
+            // Assert
+            expect(res).toEqual('success')
+        }, 15000)
+
+        test('Should throw if no success before max (max=5)', async () => {
+            // Arrange
+            let counter = 0
+            const successWhen = 10
+            const p = () => new Promise((resolve, reject) => {
+                if (counter === successWhen) {
+                    resolve('success')
+                }
+                counter++
+                reject('error')
+            })
+            // Act and Assert
+            await expect(retry(p)).rejects.toEqual('error')
+        }, 15000)
+    })
+})

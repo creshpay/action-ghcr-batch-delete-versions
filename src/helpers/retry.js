95d5755b3bfaf593d30cc36656c57384d5280351
maniolias <cedric@cresh.eu>
2022-05-13 09:17:26 +0200

    fix: log error

--- src/helpers/retry.js
+++ src/helpers/retry.js
@@ -5,10 +5,15 @@ const retry = async (fn, max=5, curr=0) => {
     try {
         return await fn()
     } catch(e) {
+        if(e.message.includes('Package version not found')){
+            return
+        }
         if (curr < max){
             curr++
             return retry(fn, max, curr)
         }
+        console.log('Error during version deletion')
+        console.log(JSON.stringify(e))
         throw e
     }
 }
